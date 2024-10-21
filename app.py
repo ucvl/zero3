2556acb824d511c436c27c5d12ebad3a2221e6f3
赵锐剑 <ruijian.zhao@ucvl.cn>
2024-10-21 15:46:16 +0800

    移动RTU实例到while外部

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import time
 from ucvl.zero3.modbus_rtu import RTU
-
-while True:
     # 然后可以使用RTU类进行操作
-    rtu_resource = RTU(
-        port='/dev/ttyS5',
-        baudrate=9600,
-        timeout=1,
-        parity='N',
-        stopbits=1,
-        bytesize=8
-        )
-    
-print(rtu_resource.read_holding_registers(DataAddress=0x00, DataCount=10, SlaveAddress=0x0D))
-print("Hello, 世界，第六个版本测试，如果有就是成功!")
-    
-time.sleep(10)
+rtu_resource = RTU(
+    port='/dev/ttyS5',
+    baudrate=9600,
+    timeout=1,
+    parity='N',
+    stopbits=1,
+    bytesize=8
+    )
+while True:
+    print(rtu_resource.read_holding_registers(DataAddress=0x00, DataCount=10, SlaveAddress=0x0D))
+    print("Hello, 世界，第7个版本测试!")
+    time.sleep(10)

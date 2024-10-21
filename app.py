7826050dcf5050d870ed1920d2cea6b55c33f552
赵锐剑 <ruijian.zhao@ucvl.cn>
2024-10-21 14:46:02 +0800

    加入ModbusRTU

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ while True:
         stopbits=1,
         bytesize=8
         )
-
+    
 print(rtu_resource.read_holding_registers(DataAddress=0x00, DataCount=10, SlaveAddress=0x0D))
 print("Hello, 世界，第四个版本测试，如果有就是成功!")
     
